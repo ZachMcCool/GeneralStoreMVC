1c0bf28481028296cb0b154b893095cf12beff0e
ZachMcCool <zachmccool@gmail.com>
2022-06-07 19:51:51 -0400

    Add project files.

---
+++ GeneralStoreMVC/Controllers/CustomerController.cs
@@ -0,0 +1,117 @@
+﻿using GeneralStoreMVC.Data;
+using GeneralStoreMVC.Models.Customer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeneralStoreMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly GeneralStoreDbContext _ctx;
+
+        public CustomerController(GeneralStoreDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            var customers = _ctx.Customers.Select(customer => new CustomerIndexModel
+            {
+                Id = customer.Id,
+                Name = customer.Name,
+                Email = customer.Email,
+
+            });
+            return View(customers);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CustomerCreateModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["ErrorMsg"] = "ModelState is Invalid";
+                return View(model);
+            }
+            _ctx.Customers.Add(new Customer
+            {
+                Name = model.Name,
+                Email = model.Email,
+            });
+            if (_ctx.SaveChanges() == 1)
+            {
+                return Redirect("/Customer");
+            }
+            TempData["ErrorMsg"] = "Unable to save to the database. Please try again later.";
+            return View(model);
+        }
+
+        // GET: customer/details/{id}
+        public IActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var customer = _ctx.Customers.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            var model = new CustomerDetailModel
+            {
+                Id = customer.Id,
+                Name = customer.Name,
+                Email = customer.Email
+            };
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Edit (int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var customer = _ctx.Customers.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            var model = new CustomerDetailModel
+            {
+                Id = customer.Id,
+                Name = customer.Name,
+                Email = customer.Email
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Edit (int id, CustomerEditModel model)
+        {
+            var customer = _ctx.Customers.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            customer.Name = model.Name;
+            customer.Email = model.Email;
+
+            if (_ctx.SaveChanges() == 1)
+            {
+                return Redirect("/customer");
+            }
+
+            ViewData["ErrorMsg"] = "Unable to save to the database. Please try again later.";
+            return View(model);
+
+        }
+    }
+}
---
+++ GeneralStoreMVC/Data/GeneralStoreDbContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace GeneralStoreMVC.Data
+{
+    public partial class GeneralStoreDbContext : DbContext
+    {
+        public GeneralStoreDbContext()
+        {
+        }
+
+        public GeneralStoreDbContext(DbContextOptions<GeneralStoreDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; } = null!;
+        public virtual DbSet<Product> Products { get; set; } = null!;
+        public virtual DbSet<Transaction> Transactions { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("name=ConnectionStrings:GSLocal");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.Property(e => e.Email).HasMaxLength(255);
+
+                entity.Property(e => e.Name).HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Name).HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Transaction>(entity =>
+            {
+                entity.Property(e => e.DateOfTransaction).HasColumnType("datetime");
+
+                entity.HasOne(d => d.Customer)
+                    .WithMany(p => p.Transactions)
+                    .HasForeignKey(d => d.CustomerId)
+                    .HasConstraintName("fk_trans_customer_id");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.Transactions)
+                    .HasForeignKey(d => d.ProductId)
+                    .HasConstraintName("fk_trans_product_id");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
